e754f0d41989b9a08fe9c530b969c5f7a6679ef5
Elizabetttz <karenelizabet2828@gmail.com>
2025-03-29 13:48:03 -0500

    conexion a supabase
    funcionando conexion base a supabase backend

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,28 @@
-import { createContext, useContext
+import { Children, createContext, useContext
     , useEffect, useState
 } from "react";
 
-import { supabase } from "../supabase/supabase.config";
+import {supabase} from "../index";
+const AuthContext = createContext();
+
+export const AuthContextProvider =({children}) =>{
+    const [user, setUser] = useState([]);
+    useEffect(()=>{
+        const {data: authListener  } = supabase.auth.onAuthStateChange(
+            async(event, session)=> {
+                console.log(event, session)
+                setUser(session?.user || null);
+        })
+        return()=>{
+            authListener.subscription;
+        };
+    },[])
+    return (
+        <AuthContext.Provider value={{user}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export const UserAuth =()=>{
+    return useContext(AuthContext)
+}
